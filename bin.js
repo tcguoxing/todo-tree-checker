54ec7f26d37cf80a68a92b15f1e6c17b694bf043
国星 孙 <tcguoxing@163.com>
2022-09-24 01:08:26 +0800

    fix default check name

--- bin.js
+++ bin.js
@@ -5,9 +5,9 @@ const todoChecker = require('./lib/index')
 const rawArgv = process.argv.slice(2); // unknown
 const args = require("minimist")(rawArgv, {});
 const searchPath = args._[0] !== undefined && args._[0].trim() !== "" ? args._[0].trim() : 'src'
-const target = args._[1] !== undefined && args._[1].trim() !== "" ? args._[1].trim() : 'todos'
+const target = args._[1] !== undefined && args._[1].trim() !== "" ? args._[1].trim() : 'todo'
 
 todoChecker.checkTodos(
   searchPath, // set default searchPath 'src'
-  target // set default target todos
+  target // set default target todo
 );
