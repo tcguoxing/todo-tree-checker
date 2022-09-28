22e25f16f4a8a61aed704810eca25e27d1f9c489
国星 孙 <tcguoxing@163.com>
2022-09-28 22:39:55 +0800

    因为不会ts语法以及vue3使用，所以暂时无法得知怎么调试

---
+++ components/processing.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { TBox, TText } from '@temir/core'
+import TSpinner from '@temir/spinner'
+
+const props = defineProps(['nowFile'])
+</script>
+  
+<template>
+  <TBox
+    :margin="5"
+    width="20"
+    border-style="round"
+    justify-content="center"
+  >
+    <TText>
+      <TText color="yellow">
+        <TSpinner />
+      </TText>
+      {{props.nowFile}} is processing...
+    </TText>
+  </TBox>
+</template>
--- main.js
+++ main.js
@@ -4,4 +4,3 @@ import { render } from '@temir/core'
 import App from './components/processing.vue'
 
 render(App)
-// render(App, { props: '这个文件' })
